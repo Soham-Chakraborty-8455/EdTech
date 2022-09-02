b1416416d7402d55bc43cf79e74b5b5d7bf74aee
Arijit Ghosh <arijitghosh120403@gmail.com>
2022-09-02 21:26:52 +0530

    commit #2
    Change the front end design for the login page

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -30,42 +30,39 @@ const Login = () => {
 	};
 
 	return (
-		<div className={styles.login_container}>
-			<div className={styles.login_form_container}>
-				<div className={styles.left}>
-					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Login to Your Account</h1>
-						<input
-							type="email"
-							placeholder="Email"
-							name="email"
-							onChange={handleChange}
-							value={data.email}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="password"
-							placeholder="Password"
-							name="password"
-							onChange={handleChange}
-							value={data.password}
-							required
-							className={styles.input}
-						/>
-						{error && <div className={styles.error_msg}>{error}</div>}
-						<button type="submit" className={styles.green_btn}>
-							Log In
-						</button>
-					</form>
-				</div>
-				<div className={styles.right}>
-					<h1>New Here ?</h1>
-					<Link to="/signup">
-						<button type="button" className={styles.white_btn}>
-							Sign Up
-						</button>
+		<div className={styles.graphic_container}>
+			<div className={styles.login_container}>
+				<div className={styles.login_form_container}>
+					<div className={styles.left}>
+						<form className={styles.form_container} onSubmit={handleSubmit}>
+							<h1>Welcome Back</h1>
+							<input
+								type="email"
+								placeholder="Email"
+								name="email"
+								onChange={handleChange}
+								value={data.email}
+								required
+								className={styles.input}
+							/>
+							<input
+								type="password"
+								placeholder="Password"
+								name="password"
+								onChange={handleChange}
+								value={data.password}
+								required
+								className={styles.input}
+							/>
+							{error && <div className={styles.error_msg}>{error}</div>}
+							<button type="submit" className={styles.green_btn}>
+								Log In
+							</button>
+						</form>
+						<Link to="/signup">
+							<h4>Create a new account</h4>
 					</Link>
+					</div>
 				</div>
 			</div>
 		</div>
